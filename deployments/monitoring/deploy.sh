dc3077869861bb6deeb7c4b9f6f8994946fe6f32
Adibov <hesamadibi80@gmail.com>
2024-07-03 15:08:02 +0330

    feat: add deployment manifests

---
+++ deployments/monitoring/deploy.sh
@@ -0,0 +1,3 @@
+kubectl apply -f configmap.yaml
+kubectl apply -f cronjob.yaml
+kubectl apply -f deployment.yaml
---
+++ deployments/postgresql/deploy.sh
@@ -0,0 +1,4 @@
+kubectl apply -f secret.yaml
+kubectl apply -f master.yaml
+kubectl apply -f slave.yaml
+kubectl apply -f service.yaml
